3cd6a7166ed3429f662bc1caf8300e0e85bca941
sean830314 <sean830314@gmail.com>
2019-12-30 21:57:20 +0800

    Commit func_decorator.py

---
+++ Design_Patterns/Structural_Design_Patterns/Decorator/func_decorator.py
@@ -0,0 +1,24 @@
+def print_func_name(func):
+    def warp_1():
+        print("Now use function '{}'".format(func.__name__))
+        func()
+    return warp_1
+
+
+def print_time(func):
+    import time
+    def warp_2():
+        print("Now the Unix time is {}".format(int(time.time())))
+        func()
+    return warp_2
+
+
+@print_func_name
+@print_time
+def dog_bark():
+    print("Bark !!!")
+
+
+
+if __name__ == "__main__":
+    dog_bark()
